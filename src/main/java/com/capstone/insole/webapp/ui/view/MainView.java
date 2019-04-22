633e54e80ef500ef5a635208b3dd972d8e636b19
agazor <AgazorSuRoot@gmail.com>
2019-04-22 12:13:14 +0300

    logger

--- src/main/java/com/capstone/insole/webapp/ui/view/MainView.java
+++ src/main/java/com/capstone/insole/webapp/ui/view/MainView.java
@@ -1,5 +1,6 @@
-package com.example.test;
+package com.capstone.insole.webapp.ui.view;
 
+import com.capstone.insole.webapp.app.HasLogger;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
@@ -9,11 +10,17 @@ import com.vaadin.flow.router.Route;
  * The main view contains a button and a click listener.
  */
 @Route("")
-public class MainView extends VerticalLayout {
+public class MainView extends VerticalLayout implements HasLogger {
 
     public MainView() {
+
+        getLogger().debug("Starting app!");
+
         Button button = new Button("Click me",
-                event -> Notification.show("Clicked!"));
+                event -> {
+                    Notification.show("Clicked!");
+                    getLogger().info("A user clicked the button!");
+                });
         add(button);
     }
 }
